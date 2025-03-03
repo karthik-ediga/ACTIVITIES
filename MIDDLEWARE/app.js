e3c48db462bbd21dda5c0967a58ac6eb9e0b753f
karthik <karthiksiri321@gmail.com>
2025-03-03 20:29:12 +0530

    six

---
+++ MIDDLEWARE/app.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const app=express();
+const port=8080;
+
+app.use("/api",(req,res,next)=>{
+    let {token}=req.query;
+    if(token==="giveaccess"){
+        return next();
+    }
+    return res.send("ACCESS DENIED!");
+});
+
+app.get("/api",(req,res)=>{
+    res.send("data");
+});
+
+app.listen(8080,()=>{
+    console.log("listening");
+});
